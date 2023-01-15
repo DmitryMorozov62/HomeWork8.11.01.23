42dba243f582b67d1ad6042de0c9e12176d97ef6
admin <62morozov.dima.90@gmail.com>
2023-01-15 21:25:55 +0300

    в задачах с процетами понял ошибку (добавлял не той перемнной) в задаче с пятнице оптимизировал. в 8 задаче убрал лишние условие.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
         task1();
         task2();
         task3();
@@ -15,11 +16,11 @@ public class Main {
         int months = 0;
         int total= 0;
         int invest = 15_000;
-        double percent = invest * 0.01;
+        double percent = 1 / 100D;
 
         while (total <= 2_459_000) {
             months++;
-            invest = invest + (int)percent;
+            total = total + (int) (total * percent);
             total = total + invest;
             System.out.println("Месяц " + months + ", сумма накоплений равна " + total + " рублей.");
         }
@@ -44,13 +45,13 @@ public class Main {
         System.out.println("Task 3");
         //writing kode for task 3
         int people = 12_000_000;
-        double natality = people / 1000 * 17;
-        double mortality = people / 1000 * 8;
+        double natality = 17;
+        double mortality = 8;
         int years = 0;
 
         while (years < 10) {
             years++;
-            people = people + (int)(natality - mortality);
+            people = people + (int)(natality - mortality) * people / 1000;
             System.out.println("Год " + years + ", численность населения составляет " + people);
         }
     }
@@ -59,12 +60,12 @@ public class Main {
         //writing kode for task 4
         int total = 0;
         int invest = 15_000;
-        double percent = invest * 0.07;
+        double percent = 7 / 100D;
         int months = 0;
 
         while (total < 12_000_000) {
             months++;
-            invest = invest + (int) percent;
+            total = total + (int) (percent * total);
             total = total + invest;
             System.out.println(months + " месяц " + total + " рублей");
         }
@@ -74,15 +75,15 @@ public class Main {
         //writing kode for task 5
         int total = 0;
         int invest = 15_000;
-        double percent = invest * 0.07;
+        double percent = 7 / 100D;
         int months = 0;
 
         while (total < 12_000_000) {
-            months++;
-            invest = invest + (int) percent;
+            total = total + (int) (total * percent);
             total = total + invest;
+            months++;
             if (months % 6 == 0) {
-            System.out.println(months + " месяц " + total + " рублей");
+                System.out.println(months + " месяц " + total + " рублей");
             }
         }
     }
@@ -91,15 +92,15 @@ public class Main {
         //writing kode for task 6
         int total = 0;
         int invest = 15_000;
-        double percent = invest * 0.07;
+        double percent = 7 / 100D;
         int months = 0;
 
         while (months < 108){
             months++;
-            invest = invest + (int)percent;
+            total = total + (int)(total * percent);
             total = total + invest;
-        if (months % 6 == 0) {
-            System.out.println(months + " месяц " + total + " рублей");
+            if (months % 6 == 0) {
+                System.out.println(months + " месяц " + total + " рублей");
             }
         }
     }
@@ -107,17 +108,14 @@ public class Main {
         System.out.println("Task 7");
         //writing kode for task 7
         int firstFriday = 3;
-        int date = 0;
 
-        while (date <= 31) {
-            date++;
-            if (date == firstFriday || date == firstFriday + 7 || date == firstFriday + 14 ||
-                    date == firstFriday + 21 || date == firstFriday + 28) {
-                System.out.println("Сегодня пятница " + date + "-е число.Необходимо подготовить отчет");
+        while (firstFriday <= 31) {
+                System.out.println("Сегодня пятница " + firstFriday + "-е число.Необходимо подготовить отчет");
+            firstFriday = firstFriday + 7;
             }
         }
 
-    }
+
     public static void task8() {
         System.out.println("Task 8 ");
         //writing kode for task 8
@@ -127,8 +125,8 @@ public class Main {
         int afterThisYears = thisYears + 100;
 
         while (years < afterThisYears) {
-            years++;
-            if (years % 79 == 0 && years > beforeThisYears && years < afterThisYears) {
+            years = years + 79;
+            if (years > beforeThisYears && years < afterThisYears) {
                 System.out.println(years);
             }
         }
